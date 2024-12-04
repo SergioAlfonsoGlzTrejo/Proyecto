2a6f5c52161c1eae30c9ca92816c03586ddee487
Jesús García Nigoche <22690042@tecvalles.mx>
2024-12-03 19:53:16 -0600

    CRUD test para comprobar la funcionalidad de la base de datos

---
+++ php/formulario.php
@@ -0,0 +1,54 @@
+<?php
+   include_once "header.php";
+?>
+
+<?php if(isset($_GET) && $_GET['error']): ?>
+   <article>
+      <?php
+      switch ($_GET['error']) {
+         case 1:
+            echo "⨉ Todos los campos son requeridos";
+         break;
+         case 2:
+            echo "⨉ La edad debe ser mayor de 18";
+         break;
+         case 3:
+            echo "⨉ El correo electrónico no es válido";
+         break;
+      }
+      ?>
+   </article>
+<?php endif; ?>
+
+<form action="agregar.php" method="post">
+   <fieldset>
+      <label for="nombre">Nombre</label>
+      <input type="text" id="nombre" name="nombre" required>
+   </fieldset>
+   <fieldset>
+      <label for="apellidos">Apellidos</label>
+      <input type="text" id="apellidos" name="apellidos" required>
+   </fieldset>
+   <fieldset>
+      <label for="edad">Edad</label>
+      <input type="number" id="edad" name="edad" min="1">
+   </fieldset>
+   <fieldset>
+      <label for="fecha_nacimiento">Fecha de nacimiento</label>
+      <input type="date" name="fecha_nacimiento" id="fecha_nacimiento">
+   </fieldset>
+   <fieldset>
+      <label for="correo">Correo electrónico</label>
+      <input type="text" id="correo" name="correo" required>
+   </fieldset>
+   <fieldset>
+      <button type="submit">
+         <i class="ph ph-paper-plane-right"></i>
+         Enviar
+      </button>
+   </fieldset>
+</form>
+
+<?php
+   include_once "footer.php";
+?>
